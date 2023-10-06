15dca0c375d5790281e8913f54395b2db48fd8a6
Forafontov Vladislav <forafontov98@gmail.com>
2023-10-06 22:46:48 +0300

    task 3

---
+++ swiftui-task3/swiftui_task3App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_task3App.swift
+//  swiftui-task3
+//
+//  Created by MacBook on 06.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_task3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
